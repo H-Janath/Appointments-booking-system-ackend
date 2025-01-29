bf328f9df4bcade605969e13650846606818c4f8
H-Janath <janathhma@gmail.com>
2025-01-29 16:22:38 +0530

    Add admin authentication middleware and secure add-doctor route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
-
+import authAdmin from '../middlewares/authAdmin.js'
 const addminRouter = express.Router();
 
-addminRouter.post('/add-doctor',upload.single('image'),addDoctor);
+addminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
 addminRouter.post('/login',loginAdmin);
 
 export default addminRouter
