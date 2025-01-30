cf9ddf73d6c9e259e9b26b8441054afb873c29c4
H-Janath <janathhma@gmail.com>
2025-01-30 15:37:56 +0530

    Add user profile management with getProfile and updateProfile endpoints

--- routes/useRouter.js
+++ routes/useRouter.js
@@ -1,12 +1,14 @@
 import express from 'express'
 
-import {registerUser,loginUser} from '../controllers/userController.js'
-
+import {registerUser,loginUser, getProfile, updateProfle} from '../controllers/userController.js'
+import authUser from '../middlewares/authUser.js';
+import upload from '../middlewares/multer.js'
 const useRouter = express.Router();
 
 useRouter.post('/register',registerUser);
 useRouter.post('/login',loginUser);
-
+useRouter.get('/get-profile',authUser,getProfile);
+useRouter.post('/update-profile',upload.single('image'),authUser,updateProfle);
 
 export default useRouter
 
