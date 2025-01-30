96186a8fc37c91deab64721b7bcfd7afbc991cdb
H-Janath <janathhma@gmail.com>
2025-01-30 11:53:33 +0530

    Fix allDoctors response and add changeAvailability endpoint for doctor availability management

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { addDoctor, allDoctors, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
+import { changeAvailability } from '../controllers/doctorController.js';
 
 
 const addminRouter = express.Router();
@@ -9,4 +10,6 @@ const addminRouter = express.Router();
 addminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
 addminRouter.post('/login',loginAdmin);
 addminRouter.post('/all-doctors',authAdmin,allDoctors);
+addminRouter.post('/change-availabity',authAdmin,changeAvailability);
+
 export default addminRouter
